140cde6a6de86df230957429faee639cdcc9c669
Remon Georgy <remon@143827.no-reply.drupal.org>
2010-08-14 11:37:37 +0000

    Added a throbber #860298

--- js/views_infinite_scroll.js
+++ js/views_infinite_scroll.js
@@ -3,13 +3,22 @@ Drupal.behaviors.views_infinite_scroll = function() {
   if($.autopager) {
     // Make sure that views ajax is disabled
     if(!Drupal.Views) {
+      $(Drupal.settings.views_infinite_scroll.pager_selector).hide();
+      var content_selector = Drupal.settings.views_infinite_scroll.content_selector;
+      var items_selector = Drupal.settings.views_infinite_scroll.items_selector;
+      var next_selector = Drupal.settings.views_infinite_scroll.next_selector;
+      var img_path = Drupal.settings.views_infinite_scroll.img_path;
+      var img = '<div id="views_infinite_scroll-ajax-loader"><img src="' + img_path + '" alt="loading..."><div>';
       $.autopager({
-        appendTo: Drupal.settings.views_infinite_scroll.content_selector,
-        content: Drupal.settings.views_infinite_scroll.items_selector,
-        link: Drupal.settings.views_infinite_scroll.next_selector,
+        appendTo: content_selector,
+        content: items_selector,
+        link: next_selector,
         page: 0,
         start: function() {
-          $(Drupal.settings.views_infinite_scroll.pager_selector).hide();
+          $(content_selector).append(img);            
+        },
+        load: function() {
+          $('div#views_infinite_scroll-ajax-loader').remove(); 
         }    
       });
     }
