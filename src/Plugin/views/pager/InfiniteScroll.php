696cb0117e7fbb3b4adfa13a75afe10d738a46a8
Sam <sam@1485048.no-reply.drupal.org>
2014-10-11 14:04:26 +0800

    Fixed use statements.

--- src/Plugin/views/pager/InfiniteScroll.php
+++ src/Plugin/views/pager/InfiniteScroll.php
@@ -7,8 +7,8 @@
 
 namespace Drupal\views_infinite_scroll\Plugin\views\pager;
 
-use Drupal\views\Plugin\views\pager\SqlBase,
-    Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\pager\SqlBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin to handle infinite scrolling.
