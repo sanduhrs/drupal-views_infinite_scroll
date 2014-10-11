191baa8a2740ce43da6e6d7cd5374a938bc68372
Sam <sam@1485048.no-reply.drupal.org>
2014-10-11 17:05:53 +0800

    Javascript to support click to load and loading message.

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -1,10 +1,37 @@
-(function($, views) {
+(function ($, views) {
   Drupal.behaviors.views_infinite_scroll = {
-    attach : function(context) {
+    attach: function (context) {
 
-      $.each(views, function(dom_id, settings) {
-        $('.view-dom-id-' + dom_id, context).once(function() {
+      $.each(views, function (unique_class, info) {
+        $(unique_class, context).once(function () {
+
           var $view = $(this);
+          var $loading_wrapper = $view.find('.loading-element-wrapper');
+          var $loading_element = $('<div/>', {class : 'test'}).text(info.options.loading_text);
+
+          $.autopager({
+            link: [info.view_class, '.infinite-scroll-wrapper .item-list .pager-next a'].join(' '),
+            content: [info.view_class, '.view-content'].join(' '),
+            autoLoad: !info.options.manual_load,
+            start: autopager_start,
+            load: autopager_load
+          });
+
+          var $load_more_button = $view.find('.load-more-button').click(function() {
+            $.autopager('load');
+          });
+
+          function autopager_start() {
+            $loading_wrapper.append($loading_element);
+          }
+
+          function autopager_load(prev, next) {
+            $loading_wrapper.empty();
+            if (next.url == undefined) {
+              $load_more_button.remove();
+            }
+          }
+
         });
       });
 
