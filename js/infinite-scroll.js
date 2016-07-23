4e76edbc44be545cb33fad5a9b52d62f8fb21de9
nod_ <nod_@598310.no-reply.drupal.org>
2016-07-23 09:43:14 +0800

    Issue #2770773 by nod_, Sam152, Anansi_boy: Support Masonry Views module with D8

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, Drupal) {
   "use strict";
 
   // Cached reference to $(window).
@@ -10,6 +10,9 @@
   // The selector for the automatic pager.
   var automatic_pager_selector = '.infinite-scroll-automatic-pager';
 
+  // The selector for both manual load and automatic pager.
+  var pager_selector = '.infinite-scroll-pager';
+
   // The selector for the automatic pager.
   var content_wrapper_selector = '.views-infinite-scroll-content-wrapper';
 
@@ -20,12 +23,27 @@
    * Insert a views infinite scroll view into the document.
    */
   $.fn.infiniteScrollInsertView = function ($new_view) {
-    var $existing_view = this;
-    var $existing_content = $existing_view.find(content_wrapper_selector).children();
-    $existing_view.css('height', $existing_view.height() + 'px');
-    $new_view.find(content_wrapper_selector).prepend($existing_content);
-    $existing_view.replaceWith($new_view);
-    $(document).trigger('infiniteScrollComplete', [$new_view, $existing_content]);
+    var current_view_id = this.selector.replace('.js-view-dom-id-', 'views_dom_id:');
+    // Get the existing ajaxViews object.
+    var view = Drupal.views.instances[current_view_id];
+    // Remove once so that the exposed form and pager are processed on
+    // behavior attach.
+    view.$view.removeOnce('ajax-pager');
+    view.$exposed_form.removeOnce('exposed-form');
+    // Make sure infinite scroll can be reinitialized.
+    var $existing_pager = view.$view.find(pager_selector);
+    $existing_pager.removeOnce('infinite-scroll');
+
+    var $new_rows = $new_view.find(content_wrapper_selector).children();
+    var $new_pager = $new_view.find('.js-pager__items');
+
+    // Add the new rows to existing view.
+    view.$view.find(content_wrapper_selector).append($new_rows);
+    // Replace the pager link with the new link and ajaxPageState values.
+    $existing_pager.replaceWith($new_pager);
+
+    // Run views and VIS behaviors.
+    Drupal.attachBehaviors(view.$view[0]);
   };
 
   /**
@@ -33,7 +51,7 @@
    */
   Drupal.behaviors.views_infinite_scroll_automatic = {
     attach : function(context, settings) {
-      $(automatic_pager_selector, context).once().each(function() {
+      $(context).find(automatic_pager_selector).once('infinite-scroll').each(function() {
         var $pager = $(this);
         $pager.addClass('visually-hidden');
         $window.on(scroll_event, function() {
@@ -49,10 +67,12 @@
       // events. This is important in the case a view being refreshed for a reason
       // other than a scroll. AJAX filters are a good example of the event needing
       // to be destroyed earlier than above.
-      if ($(automatic_pager_selector, context).length != 0) {
-        $window.off(scroll_event);
+      if (trigger === 'unload') {
+        if ($(context).find(automatic_pager_selector).removeOnce('infinite-scroll').length) {
+          $window.off(scroll_event);
+        }
       }
     }
   };
 
-})(jQuery);
+})(jQuery, Drupal);
